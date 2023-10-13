3316c6b250fc52d1bede54d0832085ff88aa4d3c
megahomyak <g.megahomyak@gmail.com>
2023-10-13 19:55:25 +0500

    Rolling back, fixing app name duplication in Windows paths

--- hotfuzz_control_panel/__main__.py
+++ hotfuzz_control_panel/__main__.py
@@ -3,9 +3,8 @@ from hotfuzz import HotFuzz
 from appdirs import user_config_dir
 from pathlib import Path
 import re
-import subprocess
 
-config_path = Path(user_config_dir(appname="hotfuzz_control_panel")) / "commands"
+config_path = Path(user_config_dir(appname="hotfuzz_control_panel", appauthor=False)) / "commands"
 
 with open(config_path, encoding="utf-8") as f:
     config = f.read()
@@ -13,7 +12,10 @@ with open(config_path, encoding="utf-8") as f:
 class UnexpectedIndentation(Exception):
     pass
 
-commands_list = {}
+class ReturnCodeOfCommandIsNotNull(Exception):
+    pass
+
+commands = {}
 last_group = None
 for line in config.splitlines():
     if line.strip() == "":
@@ -21,21 +23,17 @@ for line in config.splitlines():
     if re.match(r"\s", line):
         if last_group is None:
             raise UnexpectedIndentation()
-        commands_list[last_group].append(line.lstrip())
+        commands[last_group].append(line.lstrip())
     else:
         last_group = line
-        commands_list[line] = []
+        commands[line] = []
 
-names = list(commands_list.keys())
+names = list(commands.keys())
 hotfuzz = HotFuzz(names, initially_invisible=False)
 result = hotfuzz.run()
 if result is not None:
-    commands_list = commands_list[names[result]]
-    process = subprocess.Popen(
-        os.linesep.join(commands_list),
-        shell=True,
-        close_fds=False,
-        start_new_session=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
+    commands_list = commands[names[result]]
+    for command in commands_list:
+        print(command)
+        if os.system(command) != 0:
+            raise ReturnCodeOfCommandIsNotNull()
